77d7883b654d47bf73b6d04c004d3992a4388b6f
Dalton Schwentker <d_schwentker@yahoo.com>
2023-04-28 21:30:47 -0500

    Write scraped data to CSV

--- scrape_car_data.py
+++ scrape_car_data.py
@@ -1,8 +1,12 @@
 import requests
 from bs4 import BeautifulSoup
 import time
+import csv
 
-def scrape_car_data(url):
+def scrape_car_data(page_number):
+    #Prepare the URL for inlcuding the page_number
+    url = f"https://www.cars.com/shopping/results/?list_price_max=&makes[]=&maximum_distance=30&models[]=&page={page_number}&stock_type=all&zip=63301"
+
     #Send a request to the website
     response = requests.get(url)
 
@@ -20,17 +24,28 @@ def scrape_car_data(url):
     #Find the relevant data points and extract them
     
     car_data = []
-    for i, car_listing in enumerate(soup.find_all('div', class_='vehicle-card-main js-gallery-click-card')):
+    for car_listing in soup.find_all('div', class_='vehicle-card-main js-gallery-click-card'):
         car_name = car_listing.find('h2', class_='title').text
         car_price = car_listing.find('span', class_='primary-price').text
         car_data.append([car_name, car_price])
 
-        if i >=4: #Stop after collecting 5 itmes
-            break
-
     return car_data
 
-#Test the function
-url = "https://www.cars.com/shopping/results/?stock_type=all&makes%5B%5D=&models%5B%5D=&list_price_max=&maximum_distance=30&zip=63301"
-car_data = scrape_car_data(url)
-print(car_data)
+#Scrape data from the first 5 pages.
+all_car_data = []
+for page_number in range(1, 6):
+    car_data = scrape_car_data(page_number)
+    all_car_data.extend(car_data)  # Add the data from this page to the total
+    print(f"Page {page_number} car data: {car_data}")
+    time.sleep(5)  # Wait for 5 seconds
+
+print(f"Total car data: {all_car_data}")
+
+
+#Write the data to a CSV file
+with open('car_data.csv', 'w', newline='') as f:
+    writer = csv.writer(f)
+    writer.writerow(["Car Name", "Car Price"])  # write the header
+    writer.writerows(all_car_data)  # write the data
+
+print(f"Total car data: {all_car_data}")
