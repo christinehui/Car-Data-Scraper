591b12dafc7ecbd2843a805e8e1817b31672a0f0
Dalton Schwentker <d_schwentker@yahoo.com>
2023-04-28 21:48:54 -0500

    added mileage to scraper

--- scrape_car_data.py
+++ scrape_car_data.py
@@ -4,30 +4,45 @@ import time
 import csv
 
 def scrape_car_data(page_number):
-    #Prepare the URL for inlcuding the page_number
+    # Prepare the URL for including the page_number
     url = f"https://www.cars.com/shopping/results/?list_price_max=&makes[]=&maximum_distance=30&models[]=&page={page_number}&stock_type=all&zip=63301"
 
-    #Send a request to the website
+    # Send a request to the website
     response = requests.get(url)
 
     if response.status_code != 200:
         print(f"Request to {url} returned status code {response.status_code}")
         return []
 
-    #Make sure the request was successful
-    assert response.status_code == 200
-
-
-    #Parse the HTML content of the page with BeautifulSoup
+    # Parse the HTML content of the page with BeautifulSoup
     soup = BeautifulSoup(response.content, 'html.parser')
     
-    #Find the relevant data points and extract them
-    
+    # Find the relevant data points and extract them
     car_data = []
     for car_listing in soup.find_all('div', class_='vehicle-card-main js-gallery-click-card'):
         car_name = car_listing.find('h2', class_='title').text
         car_price = car_listing.find('span', class_='primary-price').text
-        car_data.append([car_name, car_price])
+
+        # Find the URL of the individual listing
+        car_url = car_listing.find('a')['href']
+        car_url = "https://www.cars.com" + car_url  # Complete the URL if it's relative
+
+        # Send a request to the individual listing
+        car_response = requests.get(car_url)
+        car_soup = BeautifulSoup(car_response.content, 'html.parser')
+
+        # Find the mileage
+        car_specs = car_soup.find('dl', class_='fancy-description-list')
+        car_mileage = None
+        for term, desc in zip(car_specs.find_all('dt'), car_specs.find_all('dd')):
+            if term.text.strip() == 'Mileage':
+                car_mileage = desc.text.strip()
+                break
+
+        car_data.append([car_name, car_price, car_mileage])
+
+        # Optional: add a delay between requests to avoid getting blocked
+        time.sleep(1)
 
     return car_data
 
@@ -45,7 +60,5 @@ print(f"Total car data: {all_car_data}")
 #Write the data to a CSV file
 with open('car_data.csv', 'w', newline='') as f:
     writer = csv.writer(f)
-    writer.writerow(["Car Name", "Car Price"])  # write the header
+    writer.writerow(["Car Name", "Car Price", "Car Mileage"])  # write the header
     writer.writerows(all_car_data)  # write the data
-
-print(f"Total car data: {all_car_data}")
